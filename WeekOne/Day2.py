65fcfff100c5261914f756f8359890c06ffda2d4
Francis Onyango <frankonyango.w@gmail.com>
2022-01-03 00:42:37 +0300

    update questions

--- WeekOne/Day2.py
+++ WeekOne/Day2.py
@@ -26,15 +26,27 @@ print(find_it([0,1,0,1,0]))
 
 #  --------------------------------------------------------------
 """
-In a factory a printer prints labels for boxes. For one kind of boxes the printer has to use colors which, for the sake of simplicity, are named with letters from a to m.
+In a factory a printer prints labels for boxes. 
+For one kind of boxes the printer has to use colors which, 
+for the sake of simplicity, are named with letters from a to m.
 
-The colors used by the printer are recorded in a control string. For example a "good" control string would be aaabbbbhaijjjm meaning that the printer used three times color a, four times color b, one time color h then one time color a...
+The colors used by the printer are recorded in a control string. 
+For example a "good" control string would be aaabbbbhaijjjm meaning 
+that the printer used three times color a, four times color b, 
+one time color h then one time color a...
 
-Sometimes there are problems: lack of colors, technical malfunction and a "bad" control string is produced e.g. aaaxbbbbyyhwawiwjjjwwm with letters not from a to m.
+Sometimes there are problems: lack of colors, technical malfunction 
+and a "bad" control string is produced e.g. aaaxbbbbyyhwawiwjjjwwm 
+with letters not from a to m.
 
-You have to write a function *printer_error* which given a string will return the error rate of the printer as a string representing a rational whose numerator is the number of errors and the denominator the length of the control string. Don't reduce this fraction to a simpler expression.
+You have to write a function *printer_error* which given a string 
+will return the error rate of the printer as a string representing 
+a rational whose numerator is the number of errors and the denominator 
+the length of the control string. Don't reduce this fraction 
+to a simpler expression.
 
-The string has a length greater or equal to one and contains only letters from ato z.
+The string has a length greater or equal to one and contains 
+only letters from ato z.
 
 Examples:
 s="aaabbbbhaijjjm"
@@ -85,7 +97,11 @@ s = "turns out random test cases are easier than writing out basic ones"
 print(find_short(s))
 
 # -------------------------------------------------------
-"""Complete the method/function so that it converts dash/underscore delimited words into camel casing. The first word within the output should be capitalized only if the original word was capitalized (known as Upper Camel Case, also often referred to as Pascal case).
+"""Complete the method/function so that it converts 
+dash/underscore delimited words into camel casing. 
+The first word within the output should be capitalized 
+only if the original word was capitalized (known as Upper Camel Case, 
+also often referred to as Pascal case).
 
 Examples
 "the-stealth-warrior" gets converted to "theStealthWarrior"
